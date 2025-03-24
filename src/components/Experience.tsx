716fd56563c12dd861885f999e223882fbe80280
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-03-23 21:44:33 -0400

    Finished Experiences

---
+++ src/components/Experience.tsx
@@ -0,0 +1,104 @@
+import { TracingBeam } from "./ui/tracing-beam"
+
+export default function Experience() {
+    return (
+        <section className="experiences">
+            <h2>Experiences</h2>
+            <TracingBeam>
+                <div className="experience-cont">
+                    <div className="experience">
+                        <div className="top">
+                            <h6 className="position">
+                                Lead Full Stack Web Developer
+                            </h6>
+                            <div className="midsection">
+                                <span className="loc">UWDBC</span>
+                                <span className="date">Jan-2024/Present</span>
+                            </div>
+                        </div>
+                        
+                        <ul className="description list-disc">
+                            <li>Ate you are your friends</li>
+                            <li>Blah blah blah</li>
+                            <li>side details relly fucking long paragrpah idk what to put here I just wanty to make it long don’t lread it</li>
+                        </ul>
+                    </div>
+
+                    <div className="experience">
+                        <div className="top">
+                            <h6 className="position">
+                                Lead Full Stack Web Developer
+                            </h6>
+                            <div className="midsection">
+                                <span className="loc">UWDBC</span>
+                                <span className="date">Jan-2024/Present</span>
+                            </div>
+                        </div>
+                        
+                        <ul className="description list-disc">
+                            <li>Ate you are your friends</li>
+                            <li>Blah blah blah</li>
+                            <li>side details relly fucking long paragrpah idk what to put here I just wanty to make it long don’t lread it</li>
+                        </ul>
+                    </div>
+
+                    <div className="experience">
+                        <div className="top">
+                            <h6 className="position">
+                                Lead Full Stack Web Developer
+                            </h6>
+                            <div className="midsection">
+                                <span className="loc">UWDBC</span>
+                                <span className="date">Jan-2024/Present</span>
+                            </div>
+                        </div>
+                        
+                        <ul className="description list-disc">
+                            <li>Ate you are your friends</li>
+                            <li>Blah blah blah</li>
+                            <li>side details relly fucking long paragrpah idk what to put here I just wanty to make it long don’t lread it</li>
+                        </ul>
+                    </div>
+
+                    <div className="experience">
+                        <div className="top">
+                            <h6 className="position">
+                                Lead Full Stack Web Developer
+                            </h6>
+                            <div className="midsection">
+                                <span className="loc">UWDBC</span>
+                                <span className="date">Jan-2024/Present</span>
+                            </div>
+                        </div>
+                        
+                        <ul className="description list-disc">
+                            <li>Ate you are your friends</li>
+                            <li>Blah blah blah</li>
+                            <li>side details relly fucking long paragrpah idk what to put here I just wanty to make it long don’t lread it</li>
+                        </ul>
+                    </div>
+
+                    <div className="experience">
+                        <div className="top">
+                            <h6 className="position">
+                                Lead Full Stack Web Developer
+                            </h6>
+                            <div className="midsection">
+                                <span className="loc">UWDBC</span>
+                                <span className="date">Jan-2024/Present</span>
+                            </div>
+                        </div>
+                        
+                        <ul className="description list-disc">
+                            <li>Ate you are your friends</li>
+                            <li>Blah blah blah</li>
+                            <li>side details relly fucking long paragrpah idk what to put here I just wanty to make it long don’t lread it</li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </TracingBeam>
+            
+        </section>
+    )
+}
